70c394969407c44e22fb2d29507e517600b4d732
bochuan-mok@e.ntu.edu.sg <bochuan-mok@e.ntu.edu.sg>
2022-01-02 12:47:12 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const App = () => (
+  <div className='App'>
+    <h2> ReverDoor </h2>
+  </div>
+)
+
+ReactDOM.render(
+  <App />
+  , document.getElementById('root')
+)
